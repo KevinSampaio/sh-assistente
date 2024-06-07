deee9671bb94355ee02651871c69f5f9fd4c1733
KevinSampaio <kevin.sampaio@sptech.school>
2024-06-06 21:02:08 -0300

    teste

--- assistente.sh
+++ assistente.sh
@@ -54,14 +54,10 @@ version: '3.3'
 services:
   bd:
     container_name: bd-mindcore
-    image: mysql:8.0
+    image: helosalgado/atividadeso:v1
     restart: always
-    environment:
-      MYSQL_ROOT_PASSWORD: ${PASSWORD}
     ports:
       - "3307:3306"
-    volumes:
-      - ./init.sql:/docker-entrypoint-initdb.d/init.sql
 
   java_app:
     container_name: javaApp
@@ -76,137 +72,7 @@ volumes:
   mysql_data:
 EOL
 
-# Cria o arquivo init.sql
-cat <<EOL > init.sql
-CREATE DATABASE IF NOT EXISTS ${DATABASE};
-USE ${DATABASE};
-
-CREATE TABLE Empresa (
-    cnpj CHAR(14) PRIMARY KEY UNIQUE,
-    nome VARCHAR(45),
-    telefone CHAR(11)
-);
-
-CREATE TABLE Componentes (
-    idComponente INT PRIMARY KEY AUTO_INCREMENT,
-    nomeComponente VARCHAR(45),
-    quantidade INT,
-    preco DECIMAL(5,2),
-    fkEmpresa CHAR(14),
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(cnpj)
-);
-
-CREATE TABLE Sala (
-    idSala INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    andar INT,
-    fkEmpresa CHAR(14),
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(cnpj)
-);
-
-CREATE TABLE Funcionario (
-    idFunc INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    email VARCHAR(45),
-    senha VARCHAR(45),
-    telefone CHAR(11),
-    tipo VARCHAR(45),
-    CHECK (tipo IN('Empresa','Gestor','Técnico')),    
-    turno VARCHAR(20),
-    CHECK (turno IN('manha', 'tarde', 'noite')),
-    estado VARCHAR(20),
-    CHECK (estado IN('ativo', 'inativo')),
-    fkEmpresa CHAR(14),
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(cnpj)
-);
-
-CREATE TABLE Maquina (
-    hostname VARCHAR(45) PRIMARY KEY,
-    ip VARCHAR(45),
-    imagem DATE,
-    fkSala INT,
-    fkEmpresa CHAR(14),
-    FOREIGN KEY (fkSala) REFERENCES Sala(idSala),
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(cnpj)
-);
-
-CREATE TABLE Metricas (
-    idMetrica INT PRIMARY KEY AUTO_INCREMENT,
-    CompCpu INT,
-    CompDisco DOUBLE,
-    CompRam DOUBLE,
-    fkEmpresa CHAR(14),
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa(cnpj)
-);
-
-CREATE TABLE HistoricoManutencao (
-    idHistorico INT PRIMARY KEY AUTO_INCREMENT,
-    Dia DATE,
-    descricao VARCHAR(45),
-    tipo VARCHAR(45),
-    fkMaquina VARCHAR(45),
-    fkSala INT,
-    responsavel INT,
-    FOREIGN KEY (fkMaquina) REFERENCES Maquina(hostname),
-    FOREIGN KEY (fkSala) REFERENCES Sala(idSala),
-    FOREIGN KEY (responsavel) REFERENCES Funcionario(idFunc)
-);
-
-CREATE TABLE LeituraSO (
-    idSO INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    tempoAtividade BIGINT,
-    dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina VARCHAR(45),
-    FOREIGN KEY (fkMaquina) REFERENCES Maquina(hostname)
-);
-
-CREATE TABLE LeituraDisco (
-    idDisco INT PRIMARY KEY AUTO_INCREMENT,
-    total DOUBLE,
-    emUso DOUBLE,
-    disponivel DOUBLE,
-    dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina VARCHAR(45),
-    FOREIGN KEY (fkMaquina) REFERENCES Maquina(hostname)
-);
-
-CREATE TABLE LeituraJanelas (
-    idJanela INT PRIMARY KEY AUTO_INCREMENT,
-    identificador BIGINT,
-    pid INT,
-    titulo VARCHAR(120),
-    totalJanelas INT,
-    dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina VARCHAR(45),
-    FOREIGN KEY (fkMaquina) REFERENCES Maquina(hostname)
-);
-
-CREATE TABLE LeituraCPU (
-    idCPU INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    emUso DOUBLE,
-    temp DOUBLE,
-    dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina VARCHAR(45),
-    FOREIGN KEY (fkMaquina) REFERENCES Maquina(hostname)
-);
-
-CREATE TABLE LeituraMemoriaRam (
-    idRam INT PRIMARY KEY AUTO_INCREMENT,
-    emUso DOUBLE,
-    total DOUBLE,
-    dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fkMaquina VARCHAR(45),
-    FOREIGN KEY (fkMaquina) REFERENCES Maquina(hostname)
-);
-
--- Insere dados de exemplo
-INSERT INTO Empresa (cnpj, nome, telefone) VALUES ('12345678901234', 'MindCore', '11999999999');
-INSERT INTO Funcionario (nome, email, senha, telefone, tipo, turno, estado, fkEmpresa) VALUES ('Admin', 'admin@mindcore.com', 'senha123', '11999999999', 'Gestor', 'manha', 'ativo', '12345678901234');
-EOL
-
-echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) docker-compose.yml e init.sql Criados"
+echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) docker-compose.yml Criado"
 sleep 2
 
 echo "$(tput setaf 5)[Assistente Maya]: $(tput sgr0) $(tput setaf 10) Iniciando aplicação..."
@@ -220,7 +86,7 @@ executar_consulta() {
     local email="$1"
     local senha="$2"
 
-    docker exec -i bd-mindcore mysql -uroot -p"$PASSWORD" -D"$DATABASE" -e "SELECT fkEmpresa FROM Funcionario WHERE email='$email' AND senha='$senha' LIMIT 1;" | tail -n1
+    docker exec -it bd-mindcore bash -c "MYSQL_PWD=\"$PASSWORD\" mysql --batch -u root -D \"$DATABASE\" -e \"SELECT fkEmpresa FROM Funcionario WHERE email='$email' AND senha='$senha' LIMIT 1;\""
 }
 
 # Função para verificar se a consulta retornou resultado
